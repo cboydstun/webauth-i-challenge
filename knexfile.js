d51c2de007b883b05f6e2ea72beaea18bfc08a1a
Chris Boydstun <chrisboydstun@gmail.com>
2019-11-12 10:39:18 -0600

    updated knex

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,22 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './api/data/users.db'
+    },
+    migrations:{
+      directory: './api/data/migrations'
+    },
+    seeds:{
+      directory: './api/data/seeds'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
