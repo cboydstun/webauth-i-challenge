7c94548b3c19393eb689048cd60270bd8d148b5a
Chris Boydstun <chrisboydstun@gmail.com>
2019-11-12 18:26:54 -0600

    update

--- api/data/migrations/20191112135759_migration.js
+++ api/data/migrations/20191112135759_migration.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', t =>{
-        t.increments();
+        t.increments('id', 255);
         t.string('username', 255).unique()
         .notNullable();
         t.string('password', 255).notNullable();
